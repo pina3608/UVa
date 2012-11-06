4b0c1404197b3148adfde1d6aa889c2ce4a60c41
pina3608 <pina3608@vandals.uidaho.edu>
2012-11-06 00:49:04 -0800

    674.cpp added

---
+++ 674.cpp
@@ -0,0 +1,39 @@
+/*
+ * @author Santiago Pina (santiago.pina1@gmail.com)
+ *
+ */
+
+#include <iostream>
+
+#define MAX 7490
+
+using namespace std;
+
+int table[MAX];
+
+int res(int a){
+
+  if (a < 0) return 0;
+  else return table[a];
+
+}
+
+int main(){
+
+  int n;
+  int elem[5] = {1, 5, 10, 25, 50};
+  table[0] = 1;
+
+  for(int i = 1; i < MAX; i++) table[i] = 0;
+
+  for(int j = 0; j < 5; j++){
+    for (int i = 1; i < MAX; i++){
+      table[i] += res(i-elem[j]);
+    }
+  }
+
+  while (cin >> n){
+    cout << res(n) << endl;
+  }
+
+}
